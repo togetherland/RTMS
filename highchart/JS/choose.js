2c1d1d4429d6ae916f0da258ec1c6afc235f26e5
lqllyf <lqllyf1314@163.com>
2016-01-06 19:21:21 +0800

    this is the second version, in this version, i have add the CSS style into the html page.

---
+++ highchart/JS/choose.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2015/12/30.
+ */
+var add=document.getElementById("add");
+add.onclick(function(){
+    var edit1=document.getElementById("edit1");
+    edit1.style.backgroundColor="white";
+});
